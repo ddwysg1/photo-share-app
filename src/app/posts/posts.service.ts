6365cfb4ceb2d047ff24f8a10cd75d9c864e319d
ddwysg1 <ddwysg1@gmail.com>
2018-08-01 16:59:04 +0800

    node.js based backend added

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '../../../node_modules/@angular/core';
 
 import { Subject } from 'rxjs';
-import {Post} from './post.model';
+import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http.get<{message: String, posts: Post[]}>('http://localhost:3000/api/posts')
+    .subscribe((postsData) => {
+      this.posts = postsData.posts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   getPostsUpdated() {
@@ -17,8 +24,12 @@ export class PostsService {
   }
 
   addPost(title: String, text: String) {
-    const post: Post = {title, text};
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    const post: Post = {id: null, title: title, text: text};
+    this.http.post<{message: String}>('http://localhost:3000/api/posts', post)
+    .subscribe((responseData) => {
+      console.log(responseData.message );
+      this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 }
